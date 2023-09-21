e38e532516ab30cbe4873ed893bef34ab8f39551
kitagawanwan <144981066+kitagawanwan@users.noreply.github.com>
2023-09-21 04:56:23 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import random
 st.title('人生がちゃ')
 if st.button('おみくじを引く'):
-   results = ['東大理三','九大','九大','分大医学部','ででーん。。。。活水女子大学。。。。','学習院大学''熊本大学']
+   results = ['東大理三','ｓｆｃ','九大','分大医学部','ででーん。。。。活水女子大学。。。。','学習院大学''熊本大学']
    result = random.choice(results)
    st.write(f'結果:{result}')
