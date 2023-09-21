e836ec19a299ed10bcd3859eb05ee1d71b4f0d34
kitagawanwan <144981066+kitagawanwan@users.noreply.github.com>
2023-09-21 04:44:42 +0000

    ｇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import random
-st.title('じゅんたがちゃ')
+st.title('がちゃ')
 if st.button('おみくじを引く'):
    results = ['∫(5x^3+4x^2+8x)dx=','x^3-3x^2+5x+5=0を微分せよ','a1=2,a(n+1)=3an+4の一般功をを求めよ','x^2+y^2=7921の半径は?','2×7=','1+1=']
    result = random.choice(results)
